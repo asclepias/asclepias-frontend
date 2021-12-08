7b70546e13ffc390470fe95c9258b04edefdcc59
Mubdi Rahman <mubdi@sidratresearch.com>
2021-12-08 13:48:56 -0500

    Metadata Search Form Complete

--- src/routes/metadatasearch.js
+++ src/routes/metadatasearch.js
@@ -7,16 +7,165 @@ const MetadataSearch = props => {
 
     document.title = "Asclepias: Metadata Search"
 
+    const [inputMetadataSearch, setInputMetadataSearch] = useState({
+        startPubYear: "",
+        endPubYear: "",
+        keywords: "",
+        pubName: "",
+        otherSearchTerms: "",
+        errors: [],
+    })
+
+    const onChange = e => {
+        setInputMetadataSearch({
+            ...inputMetadataSearch,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log({ inputMetadataSearch })
+
+        let data = {}
+
+        if ((inputMetadataSearch.startPubYear.toString() !== "") || (inputMetadataSearch.endPubYear.toString() !== "")) {
+            data["publication_year"] = (inputMetadataSearch.startPubYear.toString() + "--" + inputMetadataSearch.endPubYear.toString())
+        }
+
+        if (inputMetadataSearch.keywords.trim() !== "") {
+            data["keyword"] = inputMetadataSearch.keywords.trim()
+        }
+
+        if (inputMetadataSearch.pubName.trim() !== "") {
+            data["publication"] = inputMetadataSearch.pubName.trim()
+        }
+
+        if (inputMetadataSearch.otherSearchTerms.trim() !== "") {
+            data["q"] = inputMetadataSearch.otherSearchTerms.trim()
+        }
+
+
+        let urlEncoded = new URLSearchParams(data)
+
+        if (validate(data)) {
+            navigate(`/metadata?${urlEncoded}`)
+        }
+        else {
+            alert("Error: You must use at least one search term")
+        }
+
+    }
+
+    const validate = (data) => {
+        console.log(data)
+        let validityStatus = true
+        if (Object.entries(data).length === 0) {
+            validityStatus = false
+        }
+
+        return validityStatus
+    }
+
     return (
 
         <div className="container">
-        <h1 className="mt-3">Metadata Search</h1>
-        <p>Find software based on the Metadata of Papers that Cite It</p>
+            <h1 className="mt-3">Metadata Search</h1>
+            <p>Find software based on the Metadata of Papers that Cite It</p>
 
 
-    </div>
+            <form onSubmit={handleSubmit} className="form-container">
+                <div className="mb-3">
+                    <label htmlFor="keyword-text" className="form-label">Citing Paper Keywords</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Type/Paste Keywords Here"
+                        id="keyword-text"
+                        onChange={onChange}
+                        value={inputMetadataSearch.keywords}
+                        name="keywords"
+                    />
+                </div>
 
-        
+                <div className="mb-3">
+                    <label htmlFor="pubName-text" className="form-label">Citing Paper Publication Name (e.g., ApJ, MNRAS)</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Type/Paste Publication/Journal Names here"
+                        id="pubName-text"
+                        onChange={onChange}
+                        value={inputMetadataSearch.pubName}
+                        name="pubName"
+                    />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="startPubYear-number" className="form-label">Citing Publication Year</label>
+                    <div className="row g-2">
+                        <div className="col-auto">
+                            <input
+                                type="number"
+                                className="form-control"
+                                placeholder="Start Year"
+                                id="startPubYear-number"
+                                onChange={onChange}
+                                value={inputMetadataSearch.startPubYear}
+                                name="startPubYear"
+                            />
+                        </div>
+                        <div className="col-auto">
+                            <input
+                                type="number"
+                                className="form-control"
+                                placeholder="End Year"
+                                id="endPubYear-number"
+                                onChange={onChange}
+                                value={inputMetadataSearch.endPubYear}
+                                name="endPubYear"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="otherSearchTerms-text" className="form-label">Other Search Terms</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Type/Paste Other Search Terms here"
+                        id="otherSearchTerms-text"
+                        onChange={onChange}
+                        value={inputMetadataSearch.otherSearchTerms}
+                        name="otherSearchTerms"
+                    />
+                </div>
+                {/* <div className="mb-3">
+                    <label htmlFor="Item2" className="form-label">Identifier Type</label>
+                    <select 
+                        className="form-select" 
+                        aria-label="Identifier Type"
+                        value={inputSearch.searchidentifiertype}
+                        onChange={onChange}
+                        name="searchidentifiertype"
+                    >
+                        <option value="choose">Choose Identifier Type</option>
+                        <option value="doi">DOI</option>
+                        <option value="ads">ADS</option>
+                        <option value="url">URL</option>
+                    </select>
+                    <div className="invalid-feedback">Choose the appropriate identifier type</div>
+                </div> */}
+                <div className="mb-3">
+                    <input type="submit" value="Search" className="btn btn-secondary" />
+                </div>
+            </form>
+
+
+        </div>
+
+
     )
 
 }
